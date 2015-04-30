23d28269496cfb3577aef0c6734091ceb6dfcaf4
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-04-30 17:05:40 +0100

    adds specHelper to refactor tests

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -16,26 +16,8 @@ describe('GitUserSearchController', function() {
     var httpBackend;
     beforeEach(inject(function($httpBackend) {
       httpBackend = $httpBackend
-      httpBackend
-        .when("GET", 'https://api.github.com/search/users?access_token=7f0d71a4e79c1512f393b77d6db94478dc94184d&q=hello')
-        .respond(
-          { items: items }
-        );
+      setHttpBackend();
     }));
-
-    var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      }, 
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
-
 
     it('displays search results', function() {
       ctrl.searchTerm = 'hello';
--- test/searchFactory.spec.js
+++ test/searchFactory.spec.js
@@ -9,26 +9,8 @@ describe('factory: Search', function() {
   }));
 
   beforeEach(inject(function($httpBackend) {
-  httpBackend = $httpBackend
-  httpBackend
-    .when("GET", 'https://api.github.com/search/users?access_token=7f0d71a4e79c1512f393b77d6db94478dc94184d&q=hello')
-    .respond(
-      { items: items }
-    );
+  setHttpBackend();
   }));
-
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    }, 
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
 
   it('responds to query', function() {
     expect(search.query).toBeDefined();
